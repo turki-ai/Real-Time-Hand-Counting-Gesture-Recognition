e91ae7271683980ee8837d1335e7ead990a5a1fb
Turki <teee979@gmail.com>
2026-09-03 18:14:13 +0300

    optimize resource usage

--- drawing.py
+++ drawing.py
@@ -6,6 +6,31 @@ import math
 import random
 import os
 
+    # signing each connection in an array
+HAND_CONNECTIONS = [
+(0, 1),  # Wrist to Thumb CMC
+(1, 2),  # Thumb CMC to MCP
+(2, 3),  # Thumb MCP to IP
+(3, 4),  # Thumb IP to TIP
+(0, 5),  # Wrist to Index MCP
+(5, 6),  # Index MCP to PIP
+(6, 7),  # Index PIP to DIP
+(7, 8),  # Index DIP to TIP
+(5, 9),  # Index MCP to Middle MCP
+(9, 10), # Middle MCP to PIP
+(10, 11),# Middle PIP to DIP
+(11, 12),# Middle DIP to TIP
+(9, 13), # Middle MCP to Ring MCP
+(13, 14),# Ring MCP to PIP
+(14, 15),# Ring PIP to DIP
+(15, 16),# Ring DIP to TIP
+(0, 17), # Wrist to Pinky MCP
+(13, 17),# Ring MCP to Pinky MCP
+(17, 18),# Pinky MCP to PIP
+(18, 19),# Pinky PIP to DIP
+(19, 20) # Pinky DIP to TIP
+]
+
 # defining the drawing function with three parameters
 # one for RGB frame, one for detection result, and an optional flag for gestures
 def draw_landmarks_on_liveStream(rgb_image, detection_result, isGesture: bool = False):
@@ -13,31 +38,6 @@ def draw_landmarks_on_liveStream(rgb_image, detection_result, isGesture: bool = 
     # if there isn't a result it will return the frame without changes
     if detection_result is None or not detection_result.hand_landmarks:
         return rgb_image
-    
-    # signing each connection in an array
-    HAND_CONNECTIONS = [
-    (0, 1),  # Wrist to Thumb CMC
-    (1, 2),  # Thumb CMC to MCP
-    (2, 3),  # Thumb MCP to IP
-    (3, 4),  # Thumb IP to TIP
-    (0, 5),  # Wrist to Index MCP
-    (5, 6),  # Index MCP to PIP
-    (6, 7),  # Index PIP to DIP
-    (7, 8),  # Index DIP to TIP
-    (5, 9),  # Index MCP to Middle MCP
-    (9, 10), # Middle MCP to PIP
-    (10, 11),# Middle PIP to DIP
-    (11, 12),# Middle DIP to TIP
-    (9, 13), # Middle MCP to Ring MCP
-    (13, 14),# Ring MCP to PIP
-    (14, 15),# Ring PIP to DIP
-    (15, 16),# Ring DIP to TIP
-    (0, 17), # Wrist to Pinky MCP
-    (13, 17),# Ring MCP to Pinky MCP
-    (17, 18),# Pinky MCP to PIP
-    (18, 19),# Pinky PIP to DIP
-    (19, 20) # Pinky DIP to TIP
-    ]
 
     # sign the gesture list into variable 
     gesture_list = detection_result.gestures
@@ -47,11 +47,10 @@ def draw_landmarks_on_liveStream(rgb_image, detection_result, isGesture: bool = 
 
     # handedness contains the classification label (right or left) each detected hand 
     handedness_list = detection_result.handedness
-    frame = np.copy(rgb_image)
 
     # signing the frames dimensions to variables
     # third variable is not needed but should be signed
-    h_frame, w_frame, _ = frame.shape
+    h_frame, w_frame, _ = rgb_image.shape
 
     # the loop that responsible for running codes on each detected hand
     for idx in range(len(hand_landmarks_list)):
@@ -67,13 +66,13 @@ def draw_landmarks_on_liveStream(rgb_image, detection_result, isGesture: bool = 
             pixel_landmarks.append((px, py))
 
             # draw a circle on each detected landmark point
-            cv2.circle(frame, (px, py), 4 ,(0, 255, 0), -1)
+            cv2.circle(rgb_image, (px, py), 4 ,(0, 255, 0), -1)
 
         # drawing a line in each connection
         for connection in HAND_CONNECTIONS:
             start_idx = connection[0]
             end_idx = connection[1]
-            cv2.line(frame, pixel_landmarks[start_idx],
+            cv2.line(rgb_image, pixel_landmarks[start_idx],
                      pixel_landmarks[end_idx], (88, 205, 54), 2)
 
         # Finding the bounding box coordinates to position the text label above the hand
@@ -87,68 +86,42 @@ def draw_landmarks_on_liveStream(rgb_image, detection_result, isGesture: bool = 
         # display the names or else just display the handedness category name
         if isGesture and gesture and gesture[0].category_name not in ["None", "Open_Palm"]:
             gesture_name = {
-              "None": "", 
-              "Closed_Fist": "Fist", 
-              "Open_Palm": "Open Palm", 
-              "Pointing_Up": "Pointing Up", 
-              "Thumb_Down": "Thumb Down", 
-              "Thumb_Up": "Thumb Up", 
-              "Victory": "Victory", 
+              "None": "",
+              "Closed_Fist": "Fist",
+              "Open_Palm": "Open Palm",
+              "Pointing_Up": "Pointing Up",
+              "Thumb_Down": "Thumb Down",
+              "Thumb_Up": "Thumb Up",
+              "Victory": "Victory",
               "ILoveYou": "I Love You"
             }
 
             raw_gesture_names = gesture[0].category_name
 
-            cv2.putText(frame, f"{gesture_name.get(raw_gesture_names, raw_gesture_names)}",
+            cv2.putText(rgb_image, f"{gesture_name.get(raw_gesture_names, raw_gesture_names)}",
                         (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
                         1, (88, 205, 54), 1, cv2.LINE_AA)
 
         else:
-            cv2.putText(frame, f"{handedness[0].category_name}",
+            cv2.putText(rgb_image, f"{handedness[0].category_name}",
                         (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
                         1, (88, 205, 54), 1, cv2.LINE_AA)
         
-    return frame
+    return rgb_image
 
 def draw_landmarks_on_video(rgb_image, detection_result, collect_data: bool = False, label: str = ""):
 
     if detection_result is None or not detection_result.hand_landmarks:
         return rgb_image
-    
-    # signing each connection in an array
-    HAND_CONNECTIONS = [
-    (0, 1),  # Wrist to Thumb CMC
-    (1, 2),  # Thumb CMC to MCP
-    (2, 3),  # Thumb MCP to IP
-    (3, 4),  # Thumb IP to TIP
-    (0, 5),  # Wrist to Index MCP
-    (5, 6),  # Index MCP to PIP
-    (6, 7),  # Index PIP to DIP
-    (7, 8),  # Index DIP to TIP
-    (5, 9),  # Index MCP to Middle MCP
-    (9, 10), # Middle MCP to PIP
-    (10, 11),# Middle PIP to DIP
-    (11, 12),# Middle DIP to TIP
-    (9, 13), # Middle MCP to Ring MCP
-    (13, 14),# Ring MCP to PIP
-    (14, 15),# Ring PIP to DIP
-    (15, 16),# Ring DIP to TIP
-    (0, 17), # Wrist to Pinky MCP
-    (13, 17),# Ring MCP to Pinky MCP
-    (17, 18),# Pinky MCP to PIP
-    (18, 19),# Pinky PIP to DIP
-    (19, 20) # Pinky DIP to TIP
-    ]
 
     # this array contain every point of every hand that is detected
     hand_landmarks_list = detection_result.hand_landmarks
     if len(hand_landmarks_list) == 0:
         return rgb_image
-    frame = np.copy(rgb_image)
 
     # signing the frames dimensions to variables
     # third variable is not needed but should be signed
-    h_frame, w_frame, _ = frame.shape
+    h_frame, w_frame, _ = rgb_image.shape
 
     # the loop that responsible for running codes on each detected hand
     for idx in range(len(hand_landmarks_list)):
@@ -167,14 +140,14 @@ def draw_landmarks_on_video(rgb_image, detection_result, collect_data: bool = Fa
             pixel_landmarks.append((px, py))
 
             # draw a circle on each detected landmark point
-            cv2.circle(frame, (px, py), 4 ,(0, 255, 0), -1)
+            cv2.circle(rgb_image, (px, py), 4 ,(0, 255, 0), -1)
 
         # drawing a line in each connection
         for connection in HAND_CONNECTIONS:
             start_idx = connection[0]
             end_idx = connection[1]
             if start_idx < len(pixel_landmarks) and end_idx < len(pixel_landmarks):
-                cv2.line(frame, pixel_landmarks[start_idx],
+                cv2.line(rgb_image, pixel_landmarks[start_idx],
                         pixel_landmarks[end_idx], (88, 205, 54), 2)
 
         # if the parameter collect_data is True then this block should
@@ -232,48 +205,22 @@ def draw_landmarks_on_video(rgb_image, detection_result, collect_data: bool = Fa
             write_row(flipped_coords)
             write_row(rotated_coords)
             write_row(scaled_coords)
-    return frame
+    return rgb_image
 
 def draw_custom_gestures(rgb_image, detection_result, the_model, gesture_labels, input_details, output_details):
 
     if detection_result is None or not detection_result.hand_landmarks:
             return rgb_image
-        
-    # signing each connection in an array
-    HAND_CONNECTIONS = [
-    (0, 1),  # Wrist to Thumb CMC
-    (1, 2),  # Thumb CMC to MCP
-    (2, 3),  # Thumb MCP to IP
-    (3, 4),  # Thumb IP to TIP
-    (0, 5),  # Wrist to Index MCP
-    (5, 6),  # Index MCP to PIP
-    (6, 7),  # Index PIP to DIP
-    (7, 8),  # Index DIP to TIP
-    (5, 9),  # Index MCP to Middle MCP
-    (9, 10), # Middle MCP to PIP
-    (10, 11),# Middle PIP to DIP
-    (11, 12),# Middle DIP to TIP
-    (9, 13), # Middle MCP to Ring MCP
-    (13, 14),# Ring MCP to PIP
-    (14, 15),# Ring PIP to DIP
-    (15, 16),# Ring DIP to TIP
-    (0, 17), # Wrist to Pinky MCP
-    (13, 17),# Ring MCP to Pinky MCP
-    (17, 18),# Pinky MCP to PIP
-    (18, 19),# Pinky PIP to DIP
-    (19, 20) # Pinky DIP to TIP
-    ]
 
     # this array contain every point of every hand that is detected
     hand_landmarks_list = detection_result.hand_landmarks
 
     # handedness contains the classification label (right or left) each detected hand 
     handedness_list = detection_result.handedness
-    frame = np.copy(rgb_image)
 
     # signing the frames dimensions to variables
     # third variable is not needed but should be signed
-    h_frame, w_frame, _ = frame.shape
+    h_frame, w_frame, _ = rgb_image.shape
 
     # prepare variables to use them later
     r_num = 0
@@ -293,14 +240,15 @@ def draw_custom_gestures(rgb_image, detection_result, the_model, gesture_labels,
             pixel_landmarks.append((px, py))
 
             # draw a circle on each detected landmark point
-            cv2.circle(frame, (px, py), 4 ,(0, 255, 0), -1)
+            cv2.circle(rgb_image, (px, py), 4 ,(0, 255, 0), -1)
 
         # drawing a line in each connection
         for connection in HAND_CONNECTIONS:
             start_idx = connection[0]
             end_idx = connection[1]
-            cv2.line(frame, pixel_landmarks[start_idx],
-                     pixel_landmarks[end_idx], (88, 205, 54), 2)
+            if start_idx < len(pixel_landmarks) and end_idx < len(pixel_landmarks):
+                cv2.line(rgb_image, pixel_landmarks[start_idx],
+                         pixel_landmarks[end_idx], (88, 205, 54), 2)
 
         # Finding the bounding box coordinates to position the text label above the hand
         x_coordinates = [pt[0] for pt in pixel_landmarks]
@@ -333,7 +281,7 @@ def draw_custom_gestures(rgb_image, detection_result, the_model, gesture_labels,
 
             if predicted_label != 'None' and confidence > 0.5:
 
-                cv2.putText(frame, f"{predicted_label} ({confidence:.2f})", 
+                cv2.putText(rgb_image, f"{predicted_label} ({confidence:.2f})", 
                             (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 
                             1, (0, 255, 0), 2)
 
@@ -346,15 +294,15 @@ def draw_custom_gestures(rgb_image, detection_result, the_model, gesture_labels,
                     l_num = current_num
                 
         else:
-            cv2.putText(frame, f"{handedness[0].category_name}",
+            cv2.putText(rgb_image, f"{handedness[0].category_name}",
                         (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
                         1, (88, 205, 54), 1, cv2.LINE_AA)
 
     # this code will calculate the total number of both gestures
     # and it will display it in the meddle of the screen
     if len(hand_landmarks_list) == 2 and l_num > 0 and r_num > 0:
-        cv2.putText(frame, f"{l_num + r_num}", 
+        cv2.putText(rgb_image, f"{l_num + r_num}", 
                     (int(w_frame/2- 30), 50), cv2.FONT_HERSHEY_SIMPLEX, 
                     1.5, (0, 255, 0), 2)
 
-    return frame
+    return rgb_image
